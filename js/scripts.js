f651f6d9959b948cafb2da17ba521cbc414dbe4e
Goncas777 <123480612+Goncas777@users.noreply.github.com>
2025-04-02 21:59:38 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const post = {
+    id: 1,
+    author: "John Doe",
+    title: "Lorem ipsum dolor sit amet",    
+    body: "hello",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+};
+
+
+const postList = [post];
+
+const navmenu = [
+    {
+        id: 1,
+        name: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        name: "Filmes Populares",
+        url: "/filmes-populares",
+    },
+    {
+        id: 3,
+        name: "Lancamentos",
+        url: "/lancamentos",
+    },
+    {
+        id: 4,
+        name: "Sobre",
+        url: "/sobre",
+    },
+]
