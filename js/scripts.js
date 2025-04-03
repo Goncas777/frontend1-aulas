dee76b52ca4aa7fa6d73aeb0e9b1888b1fea43d0
Goncas777 <123480612+Goncas777@users.noreply.github.com>
2025-04-03 21:59:04 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
-const post = {
-    id: 1,
-    author: "John Doe",
-    title: "Lorem ipsum dolor sit amet",    
-    body: "hello",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-};
+async function getMovieContent() {
+    try {
 
+        const response = await fetch("./json/data.json");
+        const data = await response.json();
 
-const postList = [post];
+        let i = 1;
+        for (const movieTitle in data["Filmes-Populares"]) {
+            const movie = data["Filmes-Populares"][movieTitle];
+            document.getElementById(`Link_Popular_${i}`).target = "_blank";
+            document.getElementById(`Link_Popular_${i}`).href = movie.link;
+            document.getElementById(`Image_Popular_${i}`).src = movie.image;
+            document.getElementById(`Title_Popular_${i}`).textContent = movie.title;
+            document.getElementById(`Genre_Popular_${i}`).textContent = movie.genre;
+            document.getElementById(`Description_Popular_${i}`).textContent = movie.description;
+            i++;
+        }
+        
+        i = 1;
+        for (const movieTitle in data["Novos-Lancamentos"]) {
+            const movie = data["Novos-Lancamentos"][movieTitle];
+            document.getElementById(`Link_New_${i}`).target = "_blank";
+            document.getElementById(`Link_New_${i}`).href = movie.link;
+            document.getElementById(`Image_New_${i}`).src = movie.image;
+            document.getElementById(`Title_New_${i}`).textContent = movie.title;
+            document.getElementById(`Genre_New_${i}`).textContent = movie.genre;
+            document.getElementById(`Description_New_${i}`).textContent = movie.description;
+            i++;
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-const navmenu = [
-    {
-        id: 1,
-        name: "Home",
-        url: "/",
-    },
-    {
-        id: 2,
-        name: "Filmes Populares",
-        url: "/filmes-populares",
-    },
-    {
-        id: 3,
-        name: "Lancamentos",
-        url: "/lancamentos",
-    },
-    {
-        id: 4,
-        name: "Sobre",
-        url: "/sobre",
-    },
-]
+getMovieContent();
